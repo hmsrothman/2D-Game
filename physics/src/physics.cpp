5f8366175c8720238f33cba9cb23a311f2f28336
Simon Rothman <ssrothman@gmail.com>
2017-05-13 16:09:16 -0500

    timeit function

--- physics/src/physics.cpp
+++ physics/src/physics.cpp
@@ -7,9 +7,33 @@
 //============================================================================
 
 #include <iostream>
+#include <ctime>
+#include <chrono>
+#include <ratio>
+
 using namespace std;
 
+void test(){
+	int x=0;
+	for(int i=0;i<10000;i++){
+		x++;
+	}
+}
+
+double timeit(void (*f)()){
+	using namespace std::chrono;
+	high_resolution_clock::time_point start = high_resolution_clock::now();
+
+	f();
+
+	high_resolution_clock::time_point end = high_resolution_clock::now();
+
+	duration<double> time_span=duration_cast<duration<double> >(end-start);
+
+	return time_span.count();
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << timeit(test) << endl;
 	return 0;
 }
